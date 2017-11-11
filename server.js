ba08815eaf404ec303bf17fbe45e7b44f8f6504c
BakerShoeMaker <info@quackenworth.com>
2017-11-11 12:39:27 -0800

    fixed parenthesis in function. waiting for tables and reservation pages.

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.get('/', (req, res) => {res.send('Hello World!')
 
 });
 
-app.get('/tables', (req, res) => {
+app.get('/tables', ( req, res) => {
     res.send("Tables page")
 });
 
-app.get('/reservation', (req, res)=> {res.send(path.join(__dirname, "reservation.html")
+app.get('/reservation', (req, res)=> {
+    res.send(path.join(__dirname, "reservation.html"))
 
 });
 
-app.get('/api/tables', (req, res)=> {res.send(path.join(__dirname, "tables.html")
-
+app.get('/api/tables', ( req, res)=> {res.send(path.join(__dirname, "tables.html"))
 });
 
 app.get('/api/waitlist', (req, res)=> {res.send("Wait List API page!")
